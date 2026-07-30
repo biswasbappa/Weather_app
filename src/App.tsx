9fac116e1e819815a837d9dda88138dd1c3605f9
Bappa Biswas <biswasbappa6294@gmail.com>
2026-07-30 13:00:27 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Weather from './components/weather';
-import './index.css'
+import React from "react";
+import Weather from "./components/weather";
+import "./index.css";
 
 const App = () => {
   return (
-      <div className="app">
-          <Weather />
-      </div>
-  )
-}
+    <div className="app">
+      <Weather />
+    </div>
+  );
+};
 
 export default App;
